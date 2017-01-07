2a0fad19ee42c6ce0d6e9f0d76470d4b57c9d193
Abdul Rehman Liaqat <10beealiaqat@seecs.edu.pk>
2017-01-08 04:36:18 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import numpy
 
 setup(
 	maintainer='Corey Lynch',
-    name='pyfm',
+    name='shit',
     packages=find_packages(),
     url='https://github.com/coreylynch/pyFM',
     cmdclass = {'build_ext': build_ext},
